2a436fa82ad8b48533c4784838f8cf88cd7720bb
Antonio Andrade <antonio.andrade@appian.com>
2017-07-13 21:38:53 -0400

    Initial Commit: lein new app generative-testing-sample

---
+++ test/generative_testing_sample/core_test.clj
@@ -0,0 +1,7 @@
+(ns generative-testing-sample.core-test
+  (:require [clojure.test :refer :all]
+            [generative-testing-sample.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
